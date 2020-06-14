29d5e820bf402b7c128825b1f869dceda0924385
932881657 <932881657@qq.com>
2020-06-14 21:36:10 +0800

    lab11

--- lab11/config.php
+++ lab11/config.php
@@ -1,7 +1,8 @@
+
 <?php
 define('DBHOST', 'localhost');
 define('DBNAME', 'art');
 define('DBUSER', 'root');
-define('DBPASS', '123456');
-define('DBCONNSTRING','mysql:host=localhost;dbname=art');
+define('DBPASS', '');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login');
 ?>
